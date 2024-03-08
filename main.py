0a5b186cf6ade607f50f70ea9233dfbfd2afe91e
AtilaLima <atilalimade@gmail.com>
2024-03-08 16:16:02 -0300

    Completa a divisão por lanes!
    Projeto completo! novas atualizações de quality-of-life podem surgir com a demanda!
    Nomes de campeões separados por Lane
    Incorporado ao randomizador de Nomes
    outras menores mudanças
    
    Ainda não possui tradução em português!
    Programa precisa de uma interface melhor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # 1.1 a random matchuper for league of legends lane phase thinking training
+# Have a random per-lane generation, and a realtime stats opener
 # Operated buy inputs in terminal.
-#! should be divided by Lanes
 #! change special names in exibition
 
 import secrets
@@ -21,10 +21,27 @@ clearConsole()
 time.sleep(0.3)
 
 #Data--------------------------------------------------------
-with open("./data/names.txt", 'r') as namesFile: #change path to correct source
-    allChamps = namesFile.read().split(",") #! champions with spaces must be shortened.
+dividedChamps = { #! divide lanes and store them here
+    'top': [],
+    'mid': [],
+    'bot': [],
+    'sup': [],
+    'jungle': [],
+};
 
-dividedChamps = {}; #! how to divide the lanes?
+files = ["bot.txt", "jungle.txt", "mid.txt", "sup.txt", "top.txt"]
+
+for filePath in files:
+    with open( ("./data/" + filePath), 'r' ) as currentFile:
+        currentChamps = currentFile.read().split(",") #! porque está carregando apenas o último?
+
+        #separar extensão do nome
+        currentFinalIndex = filePath.find(".txt")
+        storageName = filePath[0:currentFinalIndex]
+
+        for champ in currentChamps:
+            dividedChamps[storageName].append(champ)
+
 
 #Main execution functions------------------------------------
 #* random picker into local names stored
@@ -32,31 +49,31 @@ def pickMatchup( lane, totalLanes = 1 ):
     global blue_one, blue_two, red_one, red_two
 
     if lane in ["top", "mid", "jungle"]:                #*every other then botlane
-        blue_one = secrets.choice(allChamps)
-        red_one = secrets.choice(allChamps)
+        blue_one = secrets.choice(dividedChamps[lane])
+        red_one = secrets.choice(dividedChamps[lane])
 
         print(blue_one, " X ", red_one, "?")
         statsOpener(lane)
     elif lane == "bot":                                 #*generates ADC and SUP
-        blue_one = secrets.choice(allChamps)
-        blue_two = secrets.choice(allChamps)
+        blue_one = secrets.choice(dividedChamps["bot"])
+        blue_two = secrets.choice(dividedChamps["sup"])
         
-        red_one = secrets.choice(allChamps)
-        red_two = secrets.choice(allChamps)
+        red_one = secrets.choice(dividedChamps["bot"])
+        red_two = secrets.choice(dividedChamps["sup"])
         print(blue_one, " and ", blue_two, " X ", red_one, " and ", red_two, "?")
         statsOpener(lane)
     elif lane.lower() in ["5v5", "full team"]:          #*Full team generation
-        blue_one_top = secrets.choice(allChamps)
-        blue_two_jg = secrets.choice(allChamps)
-        blue_thr_mid = secrets.choice(allChamps)
-        blue_four_bot = secrets.choice(allChamps)
-        blue_fiv_bot = secrets.choice(allChamps)
+        blue_one_top = secrets.choice(dividedChamps["top"])
+        blue_two_jg = secrets.choice(dividedChamps["jungle"])
+        blue_thr_mid = secrets.choice(dividedChamps["mid"])
+        blue_four_bot = secrets.choice(dividedChamps["bot"])
+        blue_fiv_bot = secrets.choice(dividedChamps["sup"])
 
-        red_one_top = secrets.choice(allChamps)
-        red_two_jg = secrets.choice(allChamps)          #? i think opening stats for all of these would not be good... unless the IO wants it.
-        red_thr_mid = secrets.choice(allChamps)
-        red_four_bot = secrets.choice(allChamps)
-        red_fiv_bot = secrets.choice(allChamps)
+        red_one_top = secrets.choice(dividedChamps["top"])
+        red_two_jg = secrets.choice(dividedChamps["jungle"])          #? i think opening stats for all of these would not be good... unless the IO wants it.
+        red_thr_mid = secrets.choice(dividedChamps["mid"])
+        red_four_bot = secrets.choice(dividedChamps["bot"])
+        red_fiv_bot = secrets.choice(dividedChamps["sup"])
         print(f"TOP: {blue_one_top} X {red_one_top}? \
                 \nJUNGLE: {blue_two_jg} X {red_two_jg}? \
                 \nMIDDLE: {blue_thr_mid} X {red_thr_mid}? \
@@ -94,12 +111,12 @@ def statsOpener(laneToCompare):
         
         if laneToCompare in ["top", "mid", "jungle"]:
             print("\nOpening browser in 2 seconds..."); time.sleep(2.5); #!tudu: if error, should try inserting again 
-            os.system(f"start {browser} \"https://www.op.gg/champions/{blue_one}/build/{laneToCompare}?region=global&tier=diamond_plus&target_champion={red_one}\"")
+            os.system(f"start {browser} \"https://www.op.gg/champions/{blue_one}/build/{laneToCompare}?region=global&tier=all&target_champion={red_one}\"")
             print("browser opened! proceeding...")
         elif laneToCompare == "bot":
             print("\nOpening two browsers in 2 seconds..."); time.sleep(2.5); #? maybe select what region would be too slow
-            os.system(f"start {browser} \"https://www.op.gg/champions/{blue_one}/build/bot?region=global&tier=diamond_plus&target_champion={red_one}\"")
-            os.system(f"start {browser} \"https://www.op.gg/champions/{blue_two}/build/support?region=global&tier=diamond_plus&target_champion={red_two}\"")
+            os.system(f"start {browser} \"https://www.op.gg/champions/{blue_one}/build/bot?region=global&tier=all&target_champion={red_one}\"")
+            os.system(f"start {browser} \"https://www.op.gg/champions/{blue_two}/build/support?region=global&tier=all&target_champion={red_two}\"")
             print("browser opened! proceeding...")
         
         print("")
@@ -108,11 +125,12 @@ def statsOpener(laneToCompare):
     else: 
         raise Exception("Invalid option in statsOpener! Code 4")
 
-print("Insert bellow some infos to begin the your code execution."); time.sleep(1)
+print("Insert bellow some infos to begin the code execution."); time.sleep(1)
 print("Inputting a blank space in the lane section or a \"0\" in the repetition section will stop the execution."); time.sleep(1)
 print("----------------------")
 
 
+#Repeatition and usage---------------------------------------
 while True:
     iterationCounter = 1 #for repeating
 
